206732a9161d409e4fc75e102448624a9f888d0f
SudhanshuYadav174 <146558312+SudhanshuYadav174@users.noreply.github.com>
2025-05-12 01:09:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-
+const socket = io("https://chat-2utk.onrender.com");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
